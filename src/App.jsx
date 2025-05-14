30a0b739b61be05a18285ccd2f91c8dd41e79ee0
Cristianto Sudino <sudinocristianto@gmail.com>
2025-05-13 23:02:57 -0500

    added nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import './App.css'; // import from app.css
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      
+    <div style={{
+        backgroundImage: "url('/bck.jpg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        color: 'white',
+    }}
+        >
+        <h2 style={{ position: 'absolute', top: '10px', left: '10px' }}>
+        Hello my name
+        </h2>
+        </div>
+
+      
+      <nav className="top-right-nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/callme">Call Me</a></li>
+        </ul>
+      </nav>
+    </div>
+  
+    
+    
   )
 }
 
-export default App
+export default App;
